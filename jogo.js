2f924d2b69d70b88ac03eae193c3d6d4fce9070a
jessicarf18 <jessicarosalia678@gmail.com>
2020-09-15 11:03:53 -0300

    Controlando os pontos de vida

--- jogo.js
+++ jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
@@ -13,7 +14,16 @@ function posicaoRandomica() {
 
 	if (document.querySelector('#mosquito')) {
 		document.querySelector('#mosquito').remove()
+
+		if (vidas > 3) {
+			alert('game over')
+		} else {
+			document.querySelector("#v" + vidas).src = "imagens/coracao_vazio.png"
+			vidas++
+		}
 	}
+
+
 
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() * altura) - 90
@@ -30,6 +40,10 @@ function posicaoRandomica() {
 	mosquito.style.top = posicaoY + "px"
 	mosquito.style.position = "absolute"
 	mosquito.id = 'mosquito'
+
+	mosquito.onclick = function () {
+		this.remove()
+	}
 
 	document.body.appendChild(mosquito)
 }
