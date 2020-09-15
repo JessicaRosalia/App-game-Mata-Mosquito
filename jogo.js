cfc292b38321be0ef1fc151ea549060fddbf8c22
jessicarf18 <jessicarosalia678@gmail.com>
2020-09-15 10:40:09 -0300

    Controlando inserção e remoção do mosquito a cada ciclo de tempo

--- jogo.js
+++ jogo.js
@@ -11,6 +11,10 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
+	if (document.querySelector('#mosquito')) {
+		document.querySelector('#mosquito').remove()
+	}
+
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -25,6 +29,7 @@ function posicaoRandomica() {
 	mosquito.style.left = posicaoX + "px"
 	mosquito.style.top = posicaoY + "px"
 	mosquito.style.position = "absolute"
+	mosquito.id = 'mosquito'
 
 	document.body.appendChild(mosquito)
 }
