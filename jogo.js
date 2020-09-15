4aa1cb7c3ce3a65980d69bb3967f45fbd83d2e36
jessicarf18 <jessicarosalia678@gmail.com>
2020-09-15 11:33:01 -0300

    Adicionando o cronometro

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 5 //em segundos
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
@@ -9,6 +10,17 @@ function ajustaTamanhoPalcoJogo() {
 }
 
 ajustaTamanhoPalcoJogo()
+
+var cronometro = setInterval(function () {
+	tempo -= 1
+	if (tempo < 0) {
+		clearInterval(cronometro)
+		clearInterval(criaMosquito)
+		alert('vitoria')
+	} else {
+		document.querySelector("#cronometro").innerHTML = tempo
+	}
+}, 1000)
 
 function posicaoRandomica() {
 
